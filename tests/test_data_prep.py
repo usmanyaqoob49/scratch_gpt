7fda344db6f52e10bb4848e968e6f850db7c670d
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-11 22:43:00 +0500

    test

--- tests/test_data_prep.py
+++ tests/test_data_prep.py
@@ -4,5 +4,6 @@ from src.data_preparation.utils import read_verdict, convert_to_tokens, vocab_as
 verdict_text= read_verdict(path= "../data/raw/the-verdict.txt")
 print('verdict_text_lenght= ', len(verdict_text))
 
-
+verdict_text_tokens= convert_to_tokens(text= verdict_text)
+print('Number of tokens after converting text to tokens: ', len(verdict_text_tokens))
 tokenizer= SimpleTokenizer()
