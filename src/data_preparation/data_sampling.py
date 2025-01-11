80696e7ddc56a2e49979c04f3fab37649cf4db95
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-12 00:00:46 +0500

    data sampling

--- src/data_preparation/data_sampling.py
+++ src/data_preparation/data_sampling.py
@@ -1,6 +1,10 @@
 """
 As LLM gnerates next word using previous words as context, so we have to create dataset in the way that 
 we have input and output/target shifted one to right.
+
+Also it has GPTDataset class that will convert the dataset to dataset required for GPT training.
 """
 import torch
 from torch.utils.data import Dataset, DataLoader
+
+class GPTDataset()
