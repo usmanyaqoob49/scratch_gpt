16d6bc8cd2c7b78ad4e5e8c311481303504e99d5
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-11 22:42:04 +0500

    test

--- tests/test_data_prep.py
+++ tests/test_data_prep.py
@@ -1,5 +1,8 @@
 from src.data_preparation.tokenizer import SimpleTokenizer
 from src.data_preparation.utils import read_verdict, convert_to_tokens, vocab_assign_token_id
 
-verdict_text= read_verdict(path= "")
+verdict_text= read_verdict(path= "../data/raw/the-verdict.txt")
+print('verdict_text_lenght= ', len(verdict_text))
+
+
 tokenizer= SimpleTokenizer()
