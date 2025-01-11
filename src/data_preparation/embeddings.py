832a749b7f37a50761eb52bde4d81ec544077014
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-12 02:43:40 +0500

    embeddings

--- src/data_preparation/embeddings.py
+++ src/data_preparation/embeddings.py
@@ -7,3 +7,4 @@ def create_embeddings(input_ids, vocab_size, output_dim):
     torch.manual_seed(123)
     embedding_layer= torch.nn.Embedding(vocab_size, output_dim)
     input_embeddings= embedding_layer(input_ids)
+    return input_embeddings
