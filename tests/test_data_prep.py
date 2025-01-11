fa93277c1212fc30401af89bb61ab03a77be6743
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-11 22:41:10 +0500

    test

--- tests/test_data_prep.py
+++ tests/test_data_prep.py
@@ -1,5 +1,5 @@
 from src.data_preparation.tokenizer import SimpleTokenizer
 from src.data_preparation.utils import read_verdict, convert_to_tokens, vocab_assign_token_id
 
-
+verdict_text= read_verdict()
 tokenizer= SimpleTokenizer()
