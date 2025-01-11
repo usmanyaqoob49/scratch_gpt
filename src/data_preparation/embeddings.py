92c084f4273f5722e2ef8dfcbc141f3fbf62be4a
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-12 02:54:05 +0500

    embeddings

--- src/data_preparation/embeddings.py
+++ src/data_preparation/embeddings.py
@@ -5,8 +5,11 @@ It has implementation of tokens embeddings + positional embeddings too.
 """
 import torch
 
-def create_embeddings(input_ids, vocab_size, output_dim):
+def create_embeddings(input_ids, vocab_size, context_length, output_dim):
     torch.manual_seed(123)
     embedding_layer= torch.nn.Embedding(vocab_size, output_dim)
     input_embeddings= embedding_layer(input_ids)
+
+    positional_embeddings_layer= torch.nn.Embedding(context_length, output_dim)
+    
     return input_embeddings
