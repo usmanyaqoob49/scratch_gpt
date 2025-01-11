880beb2916e69e7c2bcec87f34b30ba01ba65c37
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-11 22:18:49 +0500

    data processing

--- src/data_preparation/utils.py
+++ src/data_preparation/utils.py
@@ -7,3 +7,9 @@ def read_verdict(path):
     with open(path, "r", encoding= "utf-8") as f:
         raw_text= f.read()
     return raw_text
+
+#To convert text to tokens
+def convert_to_tokens(text):
+    tokens= re.split(r'([, .]|\s)', text)
+    result= [item for item in tokens if item.strip()] #remove white space
+    return result
