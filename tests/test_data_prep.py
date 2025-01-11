c154eba8b4c9616cd898dfa46834c2edff49c6d2
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-11 22:43:32 +0500

    test

--- tests/test_data_prep.py
+++ tests/test_data_prep.py
@@ -7,5 +7,5 @@ print('verdict_text_lenght= ', len(verdict_text))
 verdict_text_tokens= convert_to_tokens(text= verdict_text)
 print('Number of tokens after converting text to tokens: ', len(verdict_text_tokens))
 
-verdict_text_vocabulary= vocab_assign_token_id(tokens= verdict_text_tokenser)
+verdict_text_vocabulary= vocab_assign_token_id(tokens= verdict_text_tokens)
 tokenizer= SimpleTokenizer()
