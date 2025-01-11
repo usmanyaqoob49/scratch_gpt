0b365c21f4f64ab9ba94574475dd3f6a2f6830b0
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-12 02:42:50 +0500

    embeddings

--- src/data_preparation/embeddings.py
+++ src/data_preparation/embeddings.py
@@ -6,4 +6,4 @@ import torch
 def create_embeddings(input_ids, vocab_size, output_dim):
     torch.manual_seed(123)
     embedding_layer= torch.nn.Embedding(vocab_size, output_dim)
-    
+    input_embeddings= embedding_layer(input_ids)
