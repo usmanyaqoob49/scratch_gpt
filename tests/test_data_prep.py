211b807c137f58558b45f24ec28ce6b96b544e70
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-12 00:28:00 +0500

    data loader

--- tests/test_data_prep.py
+++ tests/test_data_prep.py
@@ -3,6 +3,8 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.data_preparation.tokenizer import SimpleTokenizer
 from src.data_preparation.utils import read_verdict, convert_to_tokens, vocab_assign_token_id
+from src.data_preparation.data_sampling import GPTDatasetV1
+from src.data_preparation.data_loader import DataLoader
 
 verdict_text= read_verdict(path= "./data/raw/the-verdict.txt")
 print('verdict_text_lenght= ', len(verdict_text))
@@ -20,3 +22,4 @@ print('Token ids assigned by encoder of tokenizer: ', ids)
 
 text_from_ids= tokenizer.decode(ids= ids)
 print('Text assigned from decoder using token ids', text_from_ids)
+
