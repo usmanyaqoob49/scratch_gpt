b06ee8c110b820218dd0a8b20371241ab34443e2
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-13 01:25:51 +0500

    attention mechanism-context vector

--- src/attention_mechanism/context_vector.py
+++ src/attention_mechanism/context_vector.py
@@ -14,3 +14,5 @@ def find_context_vector(inputs, query_index):
     attention_scores= torch.empty(size= inputs.shape[0])
     for i, x_i in enumerate(inputs):
         attention_scores[i]= torch.dot(query, x_i)
+
+    attention_scores_normalized= attention_scores/attention_scores.sum()
