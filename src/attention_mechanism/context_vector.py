2439dfc191989f3c6087df82e2d03dfd3ac95f5a
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-13 01:39:44 +0500

    attention mechanism-context vector

--- src/attention_mechanism/context_vector.py
+++ src/attention_mechanism/context_vector.py
@@ -11,7 +11,7 @@ query_index----> input of which we want to find context vector (that will show r
 def find_context_vector(inputs, query_index):
     query= inputs[query_index]
     
-    attention_scores= torch.empty(size= inputs.shape[0])
+    attention_scores= torch.empty(inputs.shape[0])
     for i, x_i in enumerate(inputs):
         attention_scores[i]= torch.dot(query, x_i)
 
