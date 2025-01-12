3fc14abdf11db131749a34e95550335703cd5292
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-13 01:29:13 +0500

    attention mechanism-context vector

--- src/attention_mechanism/context_vector.py
+++ src/attention_mechanism/context_vector.py
@@ -1,11 +1,6 @@
 """
 This module finds the context vector for each input.
 """
-
-"""
-inputs----> All tokens 
-query_index----> input of which we want to find context vector (that will show relation of that input will all others)
-"""
 import torch
 
 def find_context_vector(inputs, query_index):
@@ -17,4 +12,4 @@ def find_context_vector(inputs, query_index):
 
     attention_scores_normalized= torch.softmax(attention_scores, dim= 0)
 
-    
+    context_vector= 
