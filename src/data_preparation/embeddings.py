1879cd26c02441702556e86c141ed5974716d369
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-13 01:12:21 +0500

    embeddings

--- src/data_preparation/embeddings.py
+++ src/data_preparation/embeddings.py
@@ -9,7 +9,7 @@ def create_embeddings(input_ids, vocab_size, context_length, output_dim):
     torch.manual_seed(123)
     embedding_layer= torch.nn.Embedding(vocab_size, output_dim)
     input_embeddings= embedding_layer(input_ids)
-
+    print(input_embeddings.shape)
     positional_embeddings_layer= torch.nn.Embedding(context_length, output_dim)
     positional_embeddings_layer= positional_embeddings_layer(torch.arange(context_length)) #it will have numbers from 0 to input_lenght-1
 
