84460a320e2282c10c851f49c93ac232b8a3b338
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-14 23:52:06 +0500

    test context vector

--- tests/test_attentionmechanism.py
+++ tests/test_attentionmechanism.py
@@ -16,3 +16,5 @@ inputs = torch.tensor(
 
 context_vector_1= find_context_vector_query(inputs= inputs, query_index= 1)
 print('Context vector for 2nd input (journey): ', context_vector_1)
+
+#testing function that returns complete context vector of all the inputs
