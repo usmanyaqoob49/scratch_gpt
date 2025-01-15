9f1e1aeaf12b0c3232265799c1d1354d880d96b4
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-15 23:58:06 +0500

    self-attention

--- src/attention_mechanism/self_attention.py
+++ src/attention_mechanism/self_attention.py
@@ -4,7 +4,7 @@ This module has the implementation of the self attention class.---> Self attenti
 Main purpose of self attention is to find the context vector for each input.
 Each input will have its on context vector that will have info of that input and all other inputs.
 
-We find it using attention weights and inputs.
+We find it using attention weights and inputs. (Matrix Multiplication)
 """
 import torch
 
