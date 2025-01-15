4f06daeff62d16a2ac271c89aca69fada9a6ff69
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-15 23:56:04 +0500

    self-attention

--- src/attention_mechanism/self_attention.py
+++ src/attention_mechanism/self_attention.py
@@ -1,7 +1,7 @@
 """
 This module has the implementation of the self attention class.
 
-Main purpose of self attention is to find the 
+Main purpose of self attention is to find the context vector 
 """
 import torch
 
