3cd9bb804c41d254f91c1217bcb56fb775cb49fb
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-15 23:57:15 +0500

    self-attention

--- src/attention_mechanism/self_attention.py
+++ src/attention_mechanism/self_attention.py
@@ -2,7 +2,7 @@
 This module has the implementation of the self attention class.---> Self attention is finding relation with in the input (self)
 
 Main purpose of self attention is to find the context vector for each input.
-Each input will have its on context vector 
+Each input will have its on context vector that will have info of that input and all other inputs.
 """
 import torch
 
