3bb1ae842721bd838017329d06e4ffcc335d3914
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-17 21:12:30 +0500

    causal self attention

--- src/attention_mechanism/causal_self_attention.py
+++ src/attention_mechanism/causal_self_attention.py
@@ -3,5 +3,6 @@ In standard self attention that is implemented in self_attention.py, while findi
 it has access of all the attention weights current, previous and future.
 
 But in causal self attention future tokens are masked out and not shown,
-only current and previous tokens are processed together
+only current and previous tokens are processed together, this is done by masking out the attention
+
 """
