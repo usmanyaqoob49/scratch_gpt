1e05a7fb5b5163d4a197f467f2d30d45be4cad2d
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-17 21:12:39 +0500

    causal self attention

--- src/attention_mechanism/causal_self_attention.py
+++ src/attention_mechanism/causal_self_attention.py
@@ -4,5 +4,5 @@ it has access of all the attention weights current, previous and future.
 
 But in causal self attention future tokens are masked out and not shown,
 only current and previous tokens are processed together, this is done by masking out the attention
-
+weights of the future tokens.
 """
