b025b82acf7746b4d1d631643dbbdcfe116825fc
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-17 21:28:50 +0500

    causal self attention

--- src/attention_mechanism/causal_self_attention.py
+++ src/attention_mechanism/causal_self_attention.py
@@ -5,6 +5,8 @@ it has access of all the attention weights current, previous and future.
 But in causal self attention future tokens are masked out and not shown,
 only current and previous tokens are processed together, this is done by masking out the attention
 weights of the future tokens.
+
+We will also have drop out layer implemented in this to avoid overfitting.
 """
 import torch
 import torch.nn as nn
