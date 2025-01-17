e1afadc82148aa2049b5d9dfe3a6ddc4499bfbcd
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-17 21:12:00 +0500

    causal self attention

--- src/attention_mechanism/causal_self_attention.py
+++ src/attention_mechanism/causal_self_attention.py
@@ -2,5 +2,6 @@
 In standard self attention that is implemented in self_attention.py, while finding the context vector,
 it has access of all the attention weights current, previous and future.
 
-But in causal self attention future 
+But in causal self attention future tokens are masked out and not shown,
+only current 
 """
