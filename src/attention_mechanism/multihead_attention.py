49f3e24a93a752bda686201014410e73b95ffedc
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-18 15:38:06 +0500

    multihead attention

--- src/attention_mechanism/multihead_attention.py
+++ src/attention_mechanism/multihead_attention.py
@@ -52,9 +52,11 @@ class MultiHeadAttention(nn.Module):
                     [1.3,2.3]   
 
         After applying reshaping it will become like:
+                    [
                     [ [1.1],[2.1]]
                     [ [1.2],[2.2]]
-                    [ [1.3],[2.3]]​
+                    [ [1.3],[2.3]]
+                    ]
         Where now each token 1 d_out represent response of one head seprtely. I hope it makes sense:)
         """
         keys= keys.view(num_examples, num_tokens, self.num_heads, self.head_dim)
