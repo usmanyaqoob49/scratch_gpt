664598afc0edc199cc3859cbba03478abe4de084
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-18 14:04:55 +0500

    multihead attention

--- src/attention_mechanism/multihead_attention.py
+++ src/attention_mechanism/multihead_attention.py
@@ -1,4 +1,4 @@
 """
 Simply multihead attention is extension of single causal self-attention, 
-In mulit-head, mulitple single causal attention instances are combined together.
+In mulit-head, mulitple single causal attention instances are combined together each with its own weights.
 """
