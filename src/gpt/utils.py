b30e6c965ce99f80cd99b826438ad2bc12ea6762
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-20 00:41:42 +0500

    GeLU implementation

--- src/gpt/utils.py
+++ src/gpt/utils.py
@@ -15,9 +15,12 @@ GPT_CONFIG_124M= {
     'qkv_bias': False
 }
 
-#------GeLu Implementation
+#------GeLu Implementation--->This is specific implementation use in GPT-2
 class GELU(nn.Module):
     def __init__(self):
         super().__init__()
     def forward(self, x):
-        
+        return 0.5 * x * (
+            1+torch.tanh(
+                torch.sqrt(2.0/torch.pi)) *  (x + 0.044715 * torch.pow(x, 3))
+        )
