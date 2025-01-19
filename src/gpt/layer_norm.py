df5a60012519689a34ae14b1cf4fce88eb327357
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-20 00:14:43 +0500

    Layer Norm

--- src/gpt/layer_norm.py
+++ src/gpt/layer_norm.py
@@ -15,4 +15,7 @@ class LayerNorm(nn.Module):
         self.shift= nn.Parameter(torch.zeros(emb_dims))
     
     def forward(self, x):
+        means= torch.mean(x, dim= -1, keepdim= True)
+        variance= torch.var(x, dim= -1, keepdim= True)
+        
         return x
