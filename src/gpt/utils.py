99a2eb9ad3fbeee6692ba9c4a9dafef946241a64
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-19 14:33:52 +0500

    Implementing GPT-124M architecture

--- src/gpt/utils.py
+++ src/gpt/utils.py
@@ -1,1 +1,10 @@
 #Settings for GPT-2 (124M) 
+GPT_CONFIG_124M= {
+    'vocab_size': 50257, 
+    'context_length': 1024,
+    'emb_dim': 768,
+    'n_heads': 12,
+    'n_layers': 12,
+    'drop_out': 0.1, 
+    'qkv_bias': False
+}
