8d24cc68b6fbca552dc2af83ad2ba309d39dab22
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-19 14:45:30 +0500

    Implementing GPT-124M architecture

--- src/gpt/layer_norm.py
+++ src/gpt/layer_norm.py
@@ -5,5 +5,5 @@ import torch
 import torch.nn as nn
 
 class LayerNorm(nn.Module):
-    def __init__(self, normalization_shape, eps= 1e-5):
+    def __init__(self, normalized_shape, eps= 1e-5):
         super().__init__(*args, **kwargs)
