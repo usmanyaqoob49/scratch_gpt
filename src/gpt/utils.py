60d7fc24212c7f6d0b7e90c13ae86d90560ad9fa
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-20 00:41:58 +0500

    GeLU implementation

--- src/gpt/utils.py
+++ src/gpt/utils.py
@@ -19,8 +19,9 @@ GPT_CONFIG_124M= {
 class GELU(nn.Module):
     def __init__(self):
         super().__init__()
+
     def forward(self, x):
-        return 0.5 * x * (
-            1+torch.tanh(
-                torch.sqrt(2.0/torch.pi)) *  (x + 0.044715 * torch.pow(x, 3))
-        )
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
