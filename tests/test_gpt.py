31250f776a840e2371a3b31fa59dc0a508583e47
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-19 21:56:09 +0500

    Implementing GPT-124M architecture

--- tests/test_gpt.py
+++ tests/test_gpt.py
@@ -1,5 +1,7 @@
 from src.data_preparation.utils import gpt_tokeinzer
 from src.gpt.gpt_model import GPTModel
 from src.gpt.utils import GPT_CONFIG_124M
+import torch
 
+torch.manual_seed(123)
 gpt= GPTModel(cfg= GPT_CONFIG_124M)
