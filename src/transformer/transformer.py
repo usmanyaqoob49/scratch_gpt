10762f281319ee6b5538e625ece95948f876e757
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-21 23:33:29 +0500

    Coding transfomer block

--- src/transformer/transformer.py
+++ src/transformer/transformer.py
@@ -11,6 +11,14 @@ class Transformer(nn.Module):
     def __init__(self, cfg):
         super().__init__()
 
+        self.attention= MultiHeadAttention(
+            d_in= cfg['emb_dim'],
+            d_out= cfg['emb_dim'],
+            context_length= cfg['context_length'],
+            dropout= cfg['drop_out'],
+            num_heads= cfg['n_heads'],
+            qkv_bias= cfg['qkv_bias']
+        )
 
     def forward(self, x):
         return x
