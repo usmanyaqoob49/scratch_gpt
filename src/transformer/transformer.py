bd5b2f5f49e08638bad97ac43814d70c534e8e45
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-21 23:31:16 +0500

    Coding transfomer block

--- src/transformer/transformer.py
+++ src/transformer/transformer.py
@@ -5,10 +5,12 @@ We will use all the components like Multihead attention, LayerNorm, FeedForward 
 """
 import torch
 import torch.nn as nn
+from src.attention_mechanism.multihead_attention import MultiHeadAttention
 
 class Transformer(nn.Module):
     def __init__(self, cfg):
         super().__init__()
+
 
     def forward(self, x):
         return x
