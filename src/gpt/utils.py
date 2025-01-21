f0b2cf182ac0e26934cda70ea6e373c3f93591b4
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-21 23:14:35 +0500

    testing feed forward

--- src/gpt/utils.py
+++ src/gpt/utils.py
@@ -31,11 +31,13 @@ class GELU(nn.Module):
 class FeedForward(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.layers= nn.Sequential(
+        self.layers= nn.Sequential([
             nn.Linear(cfg['emb_dim'], 4* cfg['emb_dim']),
             GELU(),
             nn.Linear(4* cfg['emb_dim'], cfg['emb_dim'])
-        )
+        ])
     
     def forwrad(self, x):
-        return self.layers(x)
+        for layer in self.layers:
+            out= layer(x)
+        return out
