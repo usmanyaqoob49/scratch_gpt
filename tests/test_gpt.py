a03b010ce34487f1d9223f14e1cbab85937176a4
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-22 22:59:18 +0500

    generating text from logits

--- tests/test_gpt.py
+++ tests/test_gpt.py
@@ -52,3 +52,6 @@ print("\n\nLogits shape: ", logits.shape)
 
 total_parameters= sum(p.numel() for p in gpt.parameters())
 print("Total parameters in GPT Model:", total_parameters)
+
+
+#---------Testing generate text function that uses the gpt model to generate next token
