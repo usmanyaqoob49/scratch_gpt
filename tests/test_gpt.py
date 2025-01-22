d76ec14a218fa6d49c389264617641a46fa66e68
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-22 23:13:15 +0500

    generating text from logits testing

--- tests/test_gpt.py
+++ tests/test_gpt.py
@@ -72,4 +72,7 @@ output= generate_text(gpt_model= gpt,
                       context_size= GPT_CONFIG_124M['context_length'])
 print("Output from GPT model: ", output)
 
+decoded_text= tokenizer.decode(output)
+
+
 print('------------------------------------------------------------------\n\n')
