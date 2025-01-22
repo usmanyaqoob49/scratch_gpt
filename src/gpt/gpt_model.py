163b4cde2933cfa8c964fc7b62e934252da24537
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-22 22:33:49 +0500

    transformer block testing

--- src/gpt/gpt_model.py
+++ src/gpt/gpt_model.py
@@ -10,7 +10,7 @@ import torch
 import torch.nn as nn
 from src.data_preparation.utils import gpt_tokeinzer
 from .layer_norm import LayerNorm
-from src.transformer.transformer import Transformer
+from src.transformer.transformer_block import Transformer
 
 class GPTModel(nn.Module):
     def __init__(self, cfg):
