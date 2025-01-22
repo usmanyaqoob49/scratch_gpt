8b404ebb4b002b79848dc727385827a02e5a1093
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-22 23:06:40 +0500

    generating text from logits testing

--- tests/test_gpt.py
+++ tests/test_gpt.py
@@ -69,6 +69,6 @@ gpt.eval() #putting in evaluation mode as we are going to predict
 output= generate_text(gpt_model= gpt,
                       idx= encoded_tensor,
                       max_new_tokens= 6,
-                      )
+                      context_size= GPT_CONFIG_124M['context_size'])
 
 print('------------------------------------------------------------------\n\n')
