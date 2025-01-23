5b9147d122ca417552e6c694c7d8f2127153a8a1
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-23 23:29:06 +0500

    pretraining module

--- src/pretraining/utility.py
+++ src/pretraining/utility.py
@@ -9,3 +9,4 @@ import tiktoken
 #Function to convert text to tokens 
 def text_to_tokens(tokenizer, text):
     encoded= tokenizer.encode(text, allowed_special= {'<|endoftext|>'})
+    return encoded
