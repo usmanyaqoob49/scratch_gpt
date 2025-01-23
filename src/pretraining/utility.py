771e0db987dcf9ee71bdab29e86bfd67f62fa3ad
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-23 23:29:30 +0500

    pretraining module

--- src/pretraining/utility.py
+++ src/pretraining/utility.py
@@ -5,8 +5,10 @@ Like:
 """
 from src.gpt.utils import generate_text
 import tiktoken
+import torch
 
 #Function to convert text to tokens 
 def text_to_tokens(tokenizer, text):
     encoded= tokenizer.encode(text, allowed_special= {'<|endoftext|>'})
+    encoded_tensor= torch.ten
     return encoded
