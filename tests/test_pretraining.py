9fe225c25140eb5583fe22b106535febb207cd1f
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-23 23:49:44 +0500

    pretraining module test

--- tests/test_pretraining.py
+++ tests/test_pretraining.py
@@ -7,9 +7,9 @@ from src.gpt.utils import GPT_CONFIG_124M
 from src.data_preparation.utils import gpt_tokeinzer
 from src.gpt.utils import generate_text
 
+#-----------Testing text to token and token to text functions
 tokenizer= gpt_tokeinzer()
 gpt_model= GPTModel(cfg= GPT_CONFIG_124M)
-
 start_context = "Every effort moves you"
 token_ids_result= generate_text(
     gpt_model= gpt_model,
@@ -18,5 +18,6 @@ token_ids_result= generate_text(
     max_new_tokens= 10,
     context_size= GPT_CONFIG_124M['context_length'],
 )
-
-print("Testing text_to_tokens Function:")
+print('Output of GPT Model: ', tokens_to_text(tokenizer= tokenizer,
+                                              tokens_ids= token_ids_result))
+print("---------------------------------------------------")
