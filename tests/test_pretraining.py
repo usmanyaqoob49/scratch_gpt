7bc82314d84f28dd9717f388f2cacb77f0a0212e
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-23 23:50:13 +0500

    pretraining module test

--- tests/test_pretraining.py
+++ tests/test_pretraining.py
@@ -1,6 +1,10 @@
 """
 Test cases for different modules of pretraining.
 """
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.pretraining.utils import text_to_tokens, tokens_to_text
 from src.gpt.gpt_model import GPTModel
 from src.gpt.utils import GPT_CONFIG_124M
