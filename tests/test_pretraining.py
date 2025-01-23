1409b437e7e80051d1cfe6a4358aa942110e8632
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-23 23:44:59 +0500

    pretraining module test

--- tests/test_pretraining.py
+++ tests/test_pretraining.py
@@ -4,7 +4,11 @@ Test cases for different modules of pretraining.
 from src.pretraining.utility import text_to_tokens, tokens_to_text
 from src.gpt.gpt_model import GPTModel
 from src.gpt.utils import GPT_CONFIG_124M
+from src.data_preparation.utils import gpt_tokeinzer
 
+tokenizer= gpt_tokeinzer()
 gpt_model= GPTModel(cfg= GPT_CONFIG_124M)
+
+start_context = "Every effort moves you"
 
 print("Testing text_to_tokens Function:")
