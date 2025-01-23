211f0c14e6ebc56c71e83974ff0130225d8064db
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-23 23:40:02 +0500

    pretraining module

--- tests/test_pretraining.py
+++ tests/test_pretraining.py
@@ -2,5 +2,6 @@
 Test cases for different modules of pretraining.
 """
 from src.pretraining.utility import text_to_tokens, tokens_to_text
+from src.gpt.gpt_model import GPTModel
 
 print("Testing text_to_tokens Function:")
