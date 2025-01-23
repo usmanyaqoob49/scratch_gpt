d3185e5530dd146aacd321490e81551d84b5a118
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-23 23:35:19 +0500

    pretraining module

--- src/pretraining/utility.py
+++ src/pretraining/utility.py
@@ -17,4 +17,4 @@ def text_to_tokens(tokenizer, text):
 #Function to convert tokens to text
 def tokens_to_text(tokenizer, tokens_ids):
     flat= tokens_ids.sequeeze(0)
-    
+    text_tokens= tokenizer.decode(flat.tolist())
