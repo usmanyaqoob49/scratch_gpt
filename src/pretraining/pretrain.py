0d3f3bf5426d8d2c18d48db7b59f3470a2803f7e
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 23:04:25 +0500

    pretrain

--- src/pretraining/pretrain.py
+++ src/pretraining/pretrain.py
@@ -6,5 +6,9 @@ from .train import train_model
 from src.gpt.gpt_model import GPTModel
 from src.gpt.utils import GPT_CONFIG_124M
 
+device= {
+    'cuda' if torch.cuda.is_available() else 'cpu'
+}
 torch.manual_seed(123)
 gpt_model= GPTModel(cfg= GPT_CONFIG_124M)
+gpt_model.to(device)
