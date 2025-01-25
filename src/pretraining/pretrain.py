81475af1f4aa13e147beafbbb483929b3b0ab17e
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 23:02:33 +0500

    pretrain

--- src/pretraining/pretrain.py
+++ src/pretraining/pretrain.py
@@ -3,3 +3,4 @@ Module to pretrain the GPT-2 model using the training function in train.py-
 """
 from .train import train_model
 from src.gpt.gpt_model import GPTModel
+from src.gpt.utils import GPT_CONFIG_124M
