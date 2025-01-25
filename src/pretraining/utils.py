8bb4b87a14c69638ed95fa498a076decb76a15ae
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 20:56:54 +0500

    pretreaining the gpt

--- src/pretraining/utils.py
+++ src/pretraining/utils.py
@@ -32,4 +32,7 @@ def calculate_batch_loss(input_batch, target_batch, model, device):
 
 #Function to compute the loss of complete data loader (all batches)
 def calculate_loader_loss(data_loader, model, device, num_batches= None):
+    total_loss= 0
+    if len(data_loader)==0:
+        return float('nan')
     
