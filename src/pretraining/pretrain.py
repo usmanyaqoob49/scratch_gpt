df973b44f40bee3689231a3f068e5ae834a0ded5
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 23:13:37 +0500

    pretrain

--- src/pretraining/pretrain.py
+++ src/pretraining/pretrain.py
@@ -7,6 +7,9 @@ from src.gpt.gpt_model import GPTModel
 from src.gpt.utils import GPT_CONFIG_124M
 from .train import train_model
 from src.data_preparation.data_loader import create_data_loader_v1 
+from src.data_preparation.utils import read_txt_file
+from .utils import make_train_validation_loader
+
 torch.manual_seed(123)
 
 def pretrain_gpt(file_path, num_epochs):
@@ -21,7 +24,14 @@ def pretrain_gpt(file_path, num_epochs):
         weight_decay= 0.1
     )
 
+    text_data= read_txt_file(path= file_path)
+    training_loader, validation_loader= make_train_validation_loader(
+        text_data= text_data,
+        train_data_ratio= 0.9
+    )
     train_losses, validation_losses, tokens_seen= train_model(
         model= gpt_model,
-        train_loader= 
+        train_loader= training_loader,
+        validation_loader= validation_loader,
+        optimizer= optimizer
     )
