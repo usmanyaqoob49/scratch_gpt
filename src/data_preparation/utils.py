422a9fada614b6622aaf77fb606f6a0c048a6456
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 21:24:13 +0500

    testing loss functions

--- src/data_preparation/utils.py
+++ src/data_preparation/utils.py
@@ -6,7 +6,7 @@ import re
 import tiktoken
 
 #To read the verdict dataset
-def read_verdict(path):
+def read_txt_file(path):
     with open(path, "r", encoding= "utf-8") as f:
         raw_text= f.read()
     return raw_text
