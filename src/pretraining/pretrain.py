01b9e1be70dd9e6c0435e86ad79449c9822b6914
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 23:11:01 +0500

    pretrain

--- src/pretraining/pretrain.py
+++ src/pretraining/pretrain.py
@@ -8,20 +8,21 @@ from src.gpt.utils import GPT_CONFIG_124M
 from .train import train_model
 from src.data_preparation.data_loader import create_data_loader_v1 
 
-device= {
-    'cuda' if torch.cuda.is_available() else 'cpu'
-}
-torch.manual_seed(123)
-gpt_model= GPTModel(cfg= GPT_CONFIG_124M)
-gpt_model.to(device)
-optimizer= torch.optim.AdamW(
-    gpt_model.parameters,
-    lr= 0.0004,
-    weight_decay= 0.1
-)
-num_epochs= 10
+def pretrain_gpt(file_path)
+    device= {
+        'cuda' if torch.cuda.is_available() else 'cpu'
+    }
+    torch.manual_seed(123)
+    gpt_model= GPTModel(cfg= GPT_CONFIG_124M)
+    gpt_model.to(device)
+    optimizer= torch.optim.AdamW(
+        gpt_model.parameters,
+        lr= 0.0004,
+        weight_decay= 0.1
+    )
+    num_epochs= 10
 
-train_losses, validation_losses, tokens_seen= train_model(
-    model= gpt_model,
-    train_loader= 
-)
+    train_losses, validation_losses, tokens_seen= train_model(
+        model= gpt_model,
+        train_loader= 
+    )
