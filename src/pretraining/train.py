e9cae70b2d8a08280ac07601c8b7fe2df8d60b11
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 22:16:55 +0500

    training function

--- src/pretraining/train.py
+++ src/pretraining/train.py
@@ -2,7 +2,7 @@
 This module has function to pretrain the LLM.
 """
 import torch
-
+from .utils import calculate_batch_loss
 def train_model(model, train_loader, validation_loader,
                 optimizer, device, num_epochs,
                 eval_freq, eval_iter, start_context,
