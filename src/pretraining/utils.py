e776a8b3a351264b40b906a2e7910c78a68946a6
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 20:51:07 +0500

    pretreaining the gpt

--- src/pretraining/utils.py
+++ src/pretraining/utils.py
@@ -22,4 +22,5 @@ def tokens_to_text(tokenizer, tokens_ids):
 #Function to calculate the loss of batch
 def calculate_batch_loss(input_batch, target_batch, model, device):
     input_batch= input_batch.to(device)
+    target_batch= target_batch.to(device)
     
