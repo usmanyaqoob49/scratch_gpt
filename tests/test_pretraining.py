0d35e6ed78406c95fe407f9fb23962f687be20ea
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 00:03:42 +0500

    pretreaining the gpt

--- tests/test_pretraining.py
+++ tests/test_pretraining.py
@@ -34,30 +34,6 @@ print("---------------------------------------------------")
 
 #-----------Testing loss functions
 text_data= read_txt_file(path= "./data/raw/the-verdict.txt")
-train_data_ratio= 0.90
-split_index= int(train_data_ratio * len(text_data))
-train_data_text= text_data[:split_index]
-validation_data_text= text_data[split_index:]
-
-# train_loader= create_data_loader_v1(
-#     txt= train_data_text,
-#     batch_size= 2,
-#     max_length= GPT_CONFIG_124M['context_length'],
-#     stride= GPT_CONFIG_124M['context_length'],
-#     drop_last= True,
-#     shuffle= True,
-#     num_workers= 0
-# )
-
-# validation_loader= create_data_loader_v1(
-#     txt= validation_data_text,
-#     batch_size= 2,
-#     max_length= GPT_CONFIG_124M['context_length'],
-#     stride= GPT_CONFIG_124M['context_length'],
-#     drop_last= False,
-#     shuffle= False,
-#     num_workers= 0
-# )
 train_loader, validation_loader= make_train_validation_loader(text_data= text_data,
                                                               train_data_ratio= 0.9)
 print("Train loader:")
