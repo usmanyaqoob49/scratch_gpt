dbd587c461566aedd297a230ec1a42df3add4253
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 22:14:37 +0500

    training function

--- src/pretraining/train.py
+++ src/pretraining/train.py
@@ -3,3 +3,7 @@ This module has function to pretrain the LLM.
 """
 import torch
 
+def train_model(model, train_loader, validation_loader,
+                optimizer, device, num_epochs,
+                eval_freq, eval_iter, start_context,
+                tokenizer):
