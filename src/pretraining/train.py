9132d2f1a0eb1fa153867f7c96e14767b6c68d5a
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-25 22:15:03 +0500

    training function

--- src/pretraining/train.py
+++ src/pretraining/train.py
@@ -7,3 +7,5 @@ def train_model(model, train_loader, validation_loader,
                 optimizer, device, num_epochs,
                 eval_freq, eval_iter, start_context,
                 tokenizer):
+    for epoch in range(num_epochs):
+        
