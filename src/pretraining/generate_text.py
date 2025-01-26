66fec0e466c5b38f513bc28fc2478224a1574f28
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 15:09:07 +0500

    diverse text generation function

--- src/pretraining/generate_text.py
+++ src/pretraining/generate_text.py
@@ -4,3 +4,4 @@ Function for generating text with sampling techniques, like top-k sampling, temp
 Simple text generation function is implemented in src.gpt.utils
 """
 import torch
+from .utils import temperature_scaling, top_k_sampling
