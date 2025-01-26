ad20872587f5f9760e6bf8788d9f56be0151e2b4
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 21:24:10 +0500

    downloading gpt weights

--- src/pretraining/load_openAI_weights.py
+++ src/pretraining/load_openAI_weights.py
@@ -2,6 +2,6 @@
 This module will  have function to load the gpt-2 open ai weights using helper function from gpt_downlaod.
 """
 import torch
+import tensorflow as tf
 
 def load_gpt_openai_weights(weights_path):
-    
