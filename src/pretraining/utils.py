98c49554685393bc06dd820184d81a7e1e28e591
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 22:28:32 +0500

    openai weights testing

--- src/pretraining/utils.py
+++ src/pretraining/utils.py
@@ -182,6 +182,7 @@ def top_k_sampling(logits, k):
 def load_gpt_openai_weights(weights_dir):
     tf_checkpoints= tf.train.latest_checkpoint(checkpoint_dir= weights_dir)
     settings = json.load(open(os.path.join(weights_dir, "hparams.json")))
+    print("Settings: ", settings)
     params = load_gpt2_params_from_tf_ckpt(tf_checkpoints, settings)
     return settings, params
 
