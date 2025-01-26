d6608c9959a71d4c1b3465057571b68d2cffac0e
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 22:17:20 +0500

    openai weights testing

--- tests/test_pretraining.py
+++ tests/test_pretraining.py
@@ -107,4 +107,4 @@ gpt_2_result_token_ids= generate_diverse(
     top_k= 50,
     temperature= 0.7
 )
-print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+print("Output text:\n", tokens_to_text(token_ids, tokenizer))
