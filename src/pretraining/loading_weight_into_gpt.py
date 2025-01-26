eeebc342e707fd6e2884cafa8579055b47a9ef72
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 21:52:33 +0500

    loading gpt weights

--- src/pretraining/loading_weight_into_gpt.py
+++ src/pretraining/loading_weight_into_gpt.py
@@ -7,4 +7,6 @@ from .utils import assign
 
 def loads_weight_into_gpt(gpt_model, params):
     gpt_model.pos_emb.weights= assign(gpt_model.pos_emb.weights, params['wpe'])
+    gpt_model.tok_emb.weights= assign(gpt_model.tok_emb.weights, params['wte'])
+    
 
