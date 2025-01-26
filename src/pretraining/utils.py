cc2070a98c43e90cf3ee79033f54cb98bd5635b4
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 21:29:39 +0500

    downloading gpt weights

--- src/pretraining/utils.py
+++ src/pretraining/utils.py
@@ -16,7 +16,12 @@ import os
 
 
 #Different models configurations of gpt-2
-
+gpts_models_configurations = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
 
 #Function to convert text to tokens 
 def text_to_tokens(tokenizer, text):
