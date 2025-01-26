e31470df13571ffd5c912a820196aaf30e350138
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 15:10:38 +0500

    diverse text generation function

--- src/pretraining/generate_text.py
+++ src/pretraining/generate_text.py
@@ -5,3 +5,7 @@ Simple text generation function is implemented in src.gpt.utils
 """
 import torch
 from .utils import temperature_scaling, top_k_sampling
+
+def generate_diverse(model, idx, max_new_tokens,
+                     context_size, temperature= 0.0, top_k= None, eos_id= None):
+    
