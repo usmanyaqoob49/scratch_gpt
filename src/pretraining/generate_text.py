acb3489fa50bfc9a7f535e9d102a11f073cefeb5
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 15:10:56 +0500

    diverse text generation function

--- src/pretraining/generate_text.py
+++ src/pretraining/generate_text.py
@@ -8,4 +8,5 @@ from .utils import temperature_scaling, top_k_sampling
 
 def generate_diverse(model, idx, max_new_tokens,
                      context_size, temperature= 0.0, top_k= None, eos_id= None):
-    
+    for _ in range(max_new_tokens):
+        
