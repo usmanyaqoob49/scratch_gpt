e7dea70a1d43bc155ff758efe0fe59393b5707af
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 21:58:46 +0500

    loading gpt weights

--- src/pretraining/loading_weight_into_gpt.py
+++ src/pretraining/loading_weight_into_gpt.py
@@ -10,6 +10,7 @@ def loads_weight_into_gpt(gpt_model, params):
     gpt_model.tok_emb.weights= assign(gpt_model.tok_emb.weights, params['wte'])
 
     for b in range(len(params['blocks'])):
+        q,k,v weights
         q_w, k_w, v_w= np.split((params['blocks'][b]['attn']['c_attn'])['w'], 3, axis= -1)
         gpt_model.trf_blocks[b].att.W_query.weight= assign(
             gpt_model.trf_block[b].att.W_query.weight, q_w.T
@@ -20,5 +21,6 @@ def loads_weight_into_gpt(gpt_model, params):
         gpt_model.trf_blocks[b].att.W_value.weight= assign(
             gpt_model.trf_block[b].att.W_value.weight, v_w.T
         )
-        
+        #q,k,v biases
+        q_b, k_b, v_b
 
