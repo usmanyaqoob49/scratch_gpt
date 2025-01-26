bb3a8a0bed19366fcac52a5e1355dddcdddf26cd
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 22:06:51 +0500

    loading gpt weights

--- app.py
+++ app.py
@@ -3,3 +3,19 @@
 
 
 #--------------API that will take data file path and will pretrain gpt-2 (function to be used pretrain in pretrain_gpt.py in pretraining module.)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
