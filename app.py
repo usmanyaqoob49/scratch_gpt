18a57aca164f5c0e8242b6aff96e40dfe35a4b21
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 22:08:56 +0500

    loading gpt weights

--- app.py
+++ app.py
@@ -33,4 +33,4 @@
 
 
 
-#-------------API to use the gpt model that has been loaded with the weights above
+#-------------API to use the gpt model that has been loaded with the weights above (text input text output)
