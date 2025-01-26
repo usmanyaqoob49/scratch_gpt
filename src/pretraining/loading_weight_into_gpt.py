ad67127f54157b6b3b70e6aadb2679491cae3366
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 21:51:31 +0500

    loading gpt weights

--- src/pretraining/loading_weight_into_gpt.py
+++ src/pretraining/loading_weight_into_gpt.py
@@ -5,4 +5,6 @@ import torch
 import numpy as np
 from .utils import assign
 
+def loads_weight_into_gpt(gpt_model, params):
+    gpt_model.pos_emb.weights= assign(gpt_model.pos_emb.weights, params['wpe'])
 
