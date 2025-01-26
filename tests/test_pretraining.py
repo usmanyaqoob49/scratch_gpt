4605ea15635994c1f56172b9f3b98047ee846850
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-26 22:13:14 +0500

    openai weights testing

--- tests/test_pretraining.py
+++ tests/test_pretraining.py
@@ -90,6 +90,7 @@ print("---------------------------------------------------")
 
 #-----------Testing the creative text generation function
 print('Loading weights of openAI in gpt-2 architecture that we have created and testing its text generation: ')
-openai_parameters= load_gpt2_params_from_tf_ckpt(ckpt_path= )
+openai_parameters= load_gpt2_params_from_tf_ckpt(ckpt_path= './Model/gpt-2/124M',
+                                                 settings= gpt_2_124m_configurations)
 open_gpt_model= loads_weight_into_gpt(gpt_model= gpt_model,
                                       params= )
