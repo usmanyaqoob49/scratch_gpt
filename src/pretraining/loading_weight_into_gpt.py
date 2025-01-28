bdb89d2ea69ea9a485fa86391282ca70bb8eef0f
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-28 20:52:56 +0500

    update

--- src/pretraining/loading_weight_into_gpt.py
+++ src/pretraining/loading_weight_into_gpt.py
@@ -14,7 +14,7 @@ def loads_weight_into_gpt(gpt_model, params):
         #q,k,v weights
         q_w, k_w, v_w= np.split((params['blocks'][b]['attn']['c_attn'])['w'], 3, axis= -1)
         gpt_model.transformer_blocks[b].attention_scores.W_query.weight= assign(
-            gpt_model.transformer_blocks[b].att.W_query.weight, q_w.T
+            gpt_model.transformer_blocks[b].attention_scores.W_query.weight, q_w.T
         )
         gpt_model.transformer_blocks[b].att.W_key.weight= assign(
             gpt_model.transformer_blocks[b].att.W_key.weight, k_w.T
