6dcca6c5a345e61bb98092ad323adf02cf771d8d
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-28 20:53:56 +0500

    update

--- src/pretraining/loading_weight_into_gpt.py
+++ src/pretraining/loading_weight_into_gpt.py
@@ -34,7 +34,7 @@ def loads_weight_into_gpt(gpt_model, params):
             gpt_model.transformer_blocks[b].attention_scores.W_value.bias, v_b
         )
          #getting attention output projection weights and biases
-        gpt_model.transformer_blocks[b].att.out_proj.weight = assign(
+        gpt_model.transformer_blocks[b].attention_scores.out_proj.weight = assign(
             gpt_model.transformer_blocks[b].att.out_proj.weight, 
             params["blocks"][b]["attn"]["c_proj"]["w"].T)
         gpt_model.transformer_blocks[b].att.out_proj.bias = assign(
