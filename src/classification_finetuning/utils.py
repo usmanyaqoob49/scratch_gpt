483a45c55131eb003d2ce75a6de5cb9af760d198
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-29 22:38:25 +0500

    classification finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -3,4 +3,5 @@ import pandas as pd
 #Function that will balance the classification dataset based on minimum number of classes present in it (like all class frequence will be equal to lowest repeated class)
 def balance_dataset(dataset_path, classes_column_name):
     data= pd.read_csv(dataset_path)
-    data_count_df= pd.DataFrame(data[classes_column_name].value_counts()) 
+    data_count_df= pd.DataFrame(data[classes_column_name].value_counts())
+     
