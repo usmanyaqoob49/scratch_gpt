cea06f3e33c125f8e8e933963448b5585a5a03c8
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-29 22:39:02 +0500

    classification finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -4,4 +4,4 @@ import pandas as pd
 def balance_dataset(dataset_path, classes_column_name):
     data= pd.read_csv(dataset_path)
     data_count_df= pd.DataFrame(data[classes_column_name].value_counts())
-     
+    lowest_frequency= data_count_df['Count'].min()
