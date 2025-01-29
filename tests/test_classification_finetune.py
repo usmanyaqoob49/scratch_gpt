6d73dcaf51ad9daacfb22f8c83e3c07ffe435d9c
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-29 22:44:19 +0500

    classification finetuning

--- tests/test_classification_finetune.py
+++ tests/test_classification_finetune.py
@@ -3,3 +3,4 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.classification_finetuning.utils import balance_dataset
 
+data= './data/raw/emotion_dataset/train.txt'
