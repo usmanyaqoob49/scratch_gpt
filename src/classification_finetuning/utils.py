169c41eb7da6612724304b3f9ed42040e7eeae3c
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-29 22:49:10 +0500

    classification finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 def balance_dataset(dataset_path, classes_column_name):
     data= pd.read_csv(dataset_path)
     data_count_df= pd.DataFrame(data[classes_column_name].value_counts())
-    lowest_frequency= data_count_df['Count'].min()
+    lowest_frequency= data_count_df['count'].min()
 
     balanced_data= data.groupby(classes_column_name).apply(lambda x: x.sample(n= lowest_frequency, random_state= 42)).reset_index(drop= True)
     return balance_dataset
