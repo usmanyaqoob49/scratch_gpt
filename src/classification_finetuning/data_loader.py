f44557b26a9b308e0f2ff63e10cd65d5fab2dbc5
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 23:31:34 +0500

    classification finetuning; creating data loaders

--- src/classification_finetuning/data_loader.py
+++ src/classification_finetuning/data_loader.py
@@ -4,7 +4,7 @@ This module has a function that process the dataset dataframe and then make a da
 from .data_prep import ClassDataset
 from torch.utils.data import DataLoader
 from src.data_preparation.utils import gpt_tokenizer
-from .utils import map_classes
+from .utils import class_mapping, map_classes
 def create_data_loaders(
         dataset,
         text_col_name,
@@ -14,7 +14,10 @@ def create_data_loaders(
         shuffle,
         drop_last
 ):
-    mapped_dataset= dataset[class_col_name].map(map_classes)
+    class_mapping_dict= class_mapping(classes_list= dataset[class_col_name].unique().tolist())
+    mapped_dataset= map_classes(dataset= dataset,
+                                class_col_name= class_col_name,
+                                class_mapping_dict= class_mapping_dict)
     tokenizer= gpt_tokenizer()
     processed_data= ClassDataset(
         data_df= dataset,
