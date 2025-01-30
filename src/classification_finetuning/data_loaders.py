524937291a44119801eef3b78a88016265166dbb
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 22:33:44 +0500

    classification finetuning; creating data loaders

--- src/classification_finetuning/data_loaders.py
+++ src/classification_finetuning/data_loaders.py
@@ -2,3 +2,5 @@
 This module has class that created data loader of csv dataset by converting text to encoded text
 and then padding the text to make them of equal lenght.
 """
+import torch
+from torch.utils.data import Dataset
