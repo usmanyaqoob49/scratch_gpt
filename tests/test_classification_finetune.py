776c2f200fc522338fd98224262e1452579ce938
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 22:22:19 +0500

    classification finetuning;

--- tests/test_classification_finetune.py
+++ tests/test_classification_finetune.py
@@ -4,8 +4,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.classification_finetuning.utils import balance_dataset, class_mapping, train_val_test_split
 import pandas as pd
 
-data= './data/processed/emotion_dataset/combined_emotions_data.csv'
-balance_data= balance_dataset(dataset_path= data, classes_column_name= 'emotion')
+data_path= './data/processed/emotion_dataset/combined_emotions_data.csv'
+balance_data= balance_dataset(dataset_path= data_path, classes_column_name= 'emotion')
 print(balance_data['emotion'].value_counts())
 
 #------Testing class lable mapping function
@@ -14,7 +14,7 @@ print('Lable mapping for the dataset: ', labels_mapping)
 
 #------Testing random split function
 data_df= pd.read_csv(data_path)
-train_set, validation_set, test_set= train_val_test_split(data=data)
+train_set, validation_set, test_set= train_val_test_split(data=data_df)
 print('shape of train set: ', train_set.shape)
 print('shape of validation set: ', validation_set.shape)
 print('shape of test set: ', test_set.shape)
