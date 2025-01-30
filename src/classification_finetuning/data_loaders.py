bc852b2c766a7114d987fe3e9cb5a70143e2dce1
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 22:41:32 +0500

    classification finetuning; creating data loaders

--- src/classification_finetuning/data_loaders.py
+++ src/classification_finetuning/data_loaders.py
@@ -9,7 +9,6 @@ import pandas as pd
 class classDataset(Dataset):
     def __init__(self, csv_file, text_col_name, labels_col_name, 
                  tokenizer, max_length= None, pad_token_id= 50256):
-        super().__init__()
         self.dataset= pd.read_csv(csv_file)
         self.encoded_texts= [
             tokenizer.encode(text_row) for text_row in self.dataset[text_col_name]
