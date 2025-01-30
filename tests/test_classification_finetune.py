0d3a0265b62b7c70a769390158e121ab7cdff2f1
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 23:01:51 +0500

    classification finetuning; creating data loaders

--- tests/test_classification_finetune.py
+++ tests/test_classification_finetune.py
@@ -25,7 +25,6 @@ print('shape of test set: ', test_set.shape)
 print('-'*50)
 
 #Testing class mapping function
-
 mapped_train_set= 
 #-------Testing data loader class
 train_dataset_loader= ClassDataset(data_df= train_set,
