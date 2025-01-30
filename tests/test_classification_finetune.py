03e9a96321cf562a6ace776ffde3685f0ba95fff
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 22:05:29 +0500

    classification finetuning;

--- tests/test_classification_finetune.py
+++ tests/test_classification_finetune.py
@@ -8,5 +8,5 @@ balance_data= balance_dataset(dataset_path= data, classes_column_name= 'emotion'
 print(balance_data['emotion'].value_counts())
 
 #------Testing class lable mapping function
-labels_mapping= class_mapping(classes_list= balance_data['emotion'].unique().to_list())
+labels_mapping= class_mapping(classes_list= balance_data['emotion'].unique().tolist())
 print('Lable mapping for the dataset: ', labels_mapping)
