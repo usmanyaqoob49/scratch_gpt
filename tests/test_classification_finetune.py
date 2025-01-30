59e195180f6d549c71571e5989e338526d4d1d1f
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 23:32:57 +0500

    classification finetuning; creating data loaders

--- tests/test_classification_finetune.py
+++ tests/test_classification_finetune.py
@@ -11,11 +11,6 @@ balance_data= balance_dataset(dataset_path= data_path, classes_column_name= 'emo
 print(balance_data['emotion'].value_counts())
 print('-'*50)
 
-#------Testing class lable mapping function
-labels_mapping= class_mapping(classes_list= balance_data['emotion'].unique().tolist())
-print('Lable mapping for the dataset: ', labels_mapping)
-print('-'*50)
-
 #------Testing random split function
 data_df= pd.read_csv(data_path)
 train_set, validation_set, test_set= train_val_test_split(data=data_df)
@@ -24,24 +19,16 @@ print('shape of validation set: ', validation_set.shape)
 print('shape of test set: ', test_set.shape)
 print('-'*50)
 
-#Testing class mapping function
-mapped_train_set= map_classes(dataset= train_set, 
-                             class_col_name= 'emotion',
-                             class_mapping_dict= labels_mapping)
-mapped_validation_set= map_classes(dataset= validation_set, 
-                             class_col_name= 'emotion',
-                             class_mapping_dict= labels_mapping)
-mapped_test_set= map_classes(dataset= test_set, 
-                             class_col_name= 'emotion',
-                             class_mapping_dict= labels_mapping)
-print("After mapping : ", mapped_train_set.head(2))
-print('-'*50)
-
 #-------Testing data loader class
-train_dataset_loader= create_data_loaders(data_df= mapped_train_set,
-                                   text_col_name= 'sentence',
-                                   labels_col_name= 'emotion',
-                                   tokenizer= gpt_tokenizer())
+train_dataset_loader= create_data_loaders(
+    dataset= train_set,
+    text_col_name= 'sentence',
+    class_col_name= 'emotion',
+    batch_size= 8,
+    num_workers= 0,
+    shuffle= True,
+    drop_last= True
+)
 validation_dataset_loader= create_data_loaders(data_df=mapped_validation_set,
                                    text_col_name= 'sentence',
                                    labels_col_name= 'emotion',
