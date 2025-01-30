c319124d8890b54f7b45c795ce8671138f2056b6
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 22:11:18 +0500

    classification finetuning;

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -18,5 +18,5 @@ def class_mapping(classes_list):
 #Function to make the train, test and validation set
 def train_val_test_split(data, train_frac= 0.7, validation_frac= 0.1):
     shuffled_data= data.sample(frac= 1, random_state= 42).reset_index(drop= True)
-    train_split_index= len(data) * train_frac
-    
+    train_split_index= int(len(data) * train_frac)
+    validatoin_split_index= train_split_index + len(data) *
