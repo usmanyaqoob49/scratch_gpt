8e81b9b4304bbb2f676840ff0c23a99824b68694
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 22:35:48 +0500

    classification finetuning; creating data loaders

--- src/classification_finetuning/data_loaders.py
+++ src/classification_finetuning/data_loaders.py
@@ -4,6 +4,7 @@ and then padding the text to make them of equal lenght.
 """
 import torch
 from torch.utils.data import Dataset
+import pandas as pd
 
 class classDataset(Dataset):
     def __init__(self, csv_file, text_col_name, labels_col_name, 
