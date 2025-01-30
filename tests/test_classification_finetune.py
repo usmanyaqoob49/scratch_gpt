b272540b117acebbe09818c194cc8c4f6205b6b0
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 23:34:50 +0500

    classification finetuning; creating data loaders

--- tests/test_classification_finetune.py
+++ tests/test_classification_finetune.py
@@ -46,5 +46,3 @@ test_dataset_loader= create_data_loaders(
     num_workers= 0,
     shuffle= False,
     drop_last= False)
-print('Train dataset sample: ', train_dataset_loader.__getitem__(2))
-print('-'*50)
