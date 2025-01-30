201066cd30b37ef186307c824e592bb2f3433dd8
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 23:23:48 +0500

    classification finetuning; creating data loaders

--- src/classification_finetuning/data_loader.py
+++ src/classification_finetuning/data_loader.py
@@ -3,9 +3,16 @@ from torch.utils.data import dataloader
 
 def create_data_loaders(
         dataset,
+        text_col_name,
+        class_col_name,
         batch_size,
         num_workers,
         shuffle,
         drop_last
 ):
-    
+    processed_data= ClassDataset(
+        data_df= dataset,
+        text_col_name= text_col_name,
+        class_col_name= class_col_name,
+        
+    )
