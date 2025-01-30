518d782361b79309f5386cbf45fac8284e1e057b
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 22:50:48 +0500

    classification finetuning; creating data loaders

--- src/classification_finetuning/data_loaders.py
+++ src/classification_finetuning/data_loaders.py
@@ -7,10 +7,10 @@ from torch.utils.data import Dataset
 import pandas as pd
 
 class classDataset(Dataset):
-    def __init__(self, csv_file, text_col_name, labels_col_name, 
+    def __init__(self, data_df, text_col_name, labels_col_name, 
                  tokenizer, max_length= None, pad_token_id= 50256):
         self.label_col_name= labels_col_name
-        self.dataset= pd.read_csv(csv_file)
+        self.dataset= data_df
         self.encoded_texts= [
             tokenizer.encode(text_row) for text_row in self.dataset[text_col_name]
         ]
