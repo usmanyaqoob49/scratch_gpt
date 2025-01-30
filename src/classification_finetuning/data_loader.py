38a143fe2ca1276c84dd090edf8a18f0dad3d37b
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 23:24:26 +0500

    classification finetuning; creating data loaders

--- src/classification_finetuning/data_loader.py
+++ src/classification_finetuning/data_loader.py
@@ -1,5 +1,6 @@
 from .data_prep import ClassDataset
 from torch.utils.data import dataloader
+from src.data_preparation.utils import gpt_tokenizer
 
 def create_data_loaders(
         dataset,
@@ -10,9 +11,10 @@ def create_data_loaders(
         shuffle,
         drop_last
 ):
+    tokenizer= gpt_
     processed_data= ClassDataset(
         data_df= dataset,
         text_col_name= text_col_name,
         class_col_name= class_col_name,
-        
+
     )
