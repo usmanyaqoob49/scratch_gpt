422640c541a4aa4910cc66aed2e9c98e42ebd62b
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-30 22:12:42 +0500

    classification finetuning;

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -21,3 +21,4 @@ def train_val_test_split(data, train_frac= 0.7, validation_frac= 0.1):
     validatoin_split_index= train_split_index + int(len(data) * validation_frac)
     train_set= data[:train_split_index]
     validation_set= data[train_split_index:validatoin_split_index]
+    test_set= 
