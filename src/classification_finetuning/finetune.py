1b536c357bebbd8012bc521fff429d7261bbac86
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-01-31 22:03:24 +0500

    classification finetuning;

--- src/classification_finetuning/finetune.py
+++ src/classification_finetuning/finetune.py
@@ -2,7 +2,7 @@
 This module has the function to finetune the gpt-2 model on the classificatio dataset.
 """
 import torch
-
+torch.manual_seed(123)
 def classification_finetune(gpt_model, num_classes):
     for params in gpt_model.parameters():
         params.required_grad()= False
