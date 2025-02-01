f9967db91f38103f3e9ecafe20b86b689e727498
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-01 23:14:21 +0500

    finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -46,4 +46,4 @@ def loader_classification_loss(data_loader, model, device, num_batches= None):
                     input_batch
                 )
                 last_token_logits= logits[:, -1, :]
-            predicted_labels= torch.argmax(last_token_logits, )
+            predicted_labels= torch.argmax(last_token_logits, dim= -1)
