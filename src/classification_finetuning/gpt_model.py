927c9f9f6723ac41c6293f7a00f88894030f754b
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-01 23:21:31 +0500

    finetuning

--- src/classification_finetuning/gpt_model.py
+++ src/classification_finetuning/gpt_model.py
@@ -6,3 +6,4 @@ import torch
 from src.gpt.gpt_model import GPTModel
 from src.pretraining.utils import load_gpt2_params_from_tf_ckpt
 from src.pretraining.loading_weight_into_gpt import loads_weight_into_gpt
+
