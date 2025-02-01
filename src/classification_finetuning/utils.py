cf0d16446bbda56c4f6d51abbce2c14477dfc5ba
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-01 23:16:59 +0500

    finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -52,4 +52,4 @@ def loader_classification_loss(data_loader, model, device, num_batches= None):
             correct_predictions+= (predicted_labels==target_batch).sum().item()
         else:
             break
-    return correct_predictions / 
+    return correct_predictions / num_examples
