64e06d9242f4ddb1d8ad369555987b544edde7aa
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-01 23:12:07 +0500

    finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import torch
 
 #Function that will balance the classification dataset based on minimum number of classes present in it (like all class frequence will be equal to lowest repeated class)
 def balance_dataset(dataset_path, classes_column_name):
