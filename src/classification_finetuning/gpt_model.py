2e373a226b657e5e4fa7370ee07e132d8648963b
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-01 23:20:35 +0500

    finetuning

--- src/classification_finetuning/gpt_model.py
+++ src/classification_finetuning/gpt_model.py
@@ -1,4 +1,4 @@
 """
 This module has function that will initialize the gpt architecture, will load openai downloaded weights for 
-gpt-2, will load these weights in the gpt architecture and in the end it will return the gpt-2 model.
+gpt-2, will load these weights in the gpt architecture and in the end it will return the gpt-2 model loaded with openai weights.
 """
