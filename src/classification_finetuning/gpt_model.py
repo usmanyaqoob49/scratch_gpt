293397e7ae035c5f09bdd13b7b219c2b29de15ce
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-01 23:21:02 +0500

    finetuning

--- src/classification_finetuning/gpt_model.py
+++ src/classification_finetuning/gpt_model.py
@@ -3,4 +3,4 @@ This module has function that will initialize the gpt architecture, will load op
 gpt-2, will load these weights in the gpt architecture and in the end it will return the gpt-2 model loaded with openai weights.
 """
 import torch
-from 
+from src.gpt.gpt_model import GPTModel
