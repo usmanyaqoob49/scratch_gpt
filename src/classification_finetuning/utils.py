91d9192e2d8f5c231d675ff64f8cb11ced0240a4
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-04 00:49:33 +0500

    finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -67,4 +67,5 @@ def batch_classification_loss(gpt_model, input_batch, target_batch, device):
 #Function to find the loss of complete loader
 def loader_classification_loss(gpt_model, loader, device):
     gpt_model.eval()
-    
+    for input_batch, target_batch in loader:
+        
