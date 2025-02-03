2e4bcfb8cacc6f19747c17349652e86cdb0cbf5a
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-03 22:20:15 +0500

    finetuning

--- src/pretraining/generate_text.py
+++ src/pretraining/generate_text.py
@@ -9,7 +9,6 @@ from .utils import temperature_scaling, top_k_sampling
 
 def generate_diverse(model, idx, max_new_tokens,
                      context_size, temperature= 0.0, top_k= None, eos_id= None):
-    print('idx: ', idx)
     for _ in range(max_new_tokens):
         idx_context= idx[:, -context_size:]
         with torch.no_grad():
