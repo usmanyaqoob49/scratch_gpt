d4f1d95d9af56fe650e06798785b020c4c3c7836
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-04 01:12:41 +0500

    finetuning

--- src/classification_finetuning/finetune.py
+++ src/classification_finetuning/finetune.py
@@ -7,5 +7,5 @@ torch.manual_seed(123)
 def finetune_model(model, train_loader, validation_loader, optimizer, device, num_epochs, eval_frequency, eval_iter):
     for epoch in num_epochs:
         model.train()
-        for i, (input_batch, target_batch) in enumerate(train_loader):
+        for input_batch, target_batch in train_loader:
             input_batch, target_batch= input_batch.to(device), target_batch.to(device)
