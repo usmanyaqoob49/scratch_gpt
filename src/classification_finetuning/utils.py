c4ebf68a3a6060102a95715af71854e3974c651e
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-04 01:09:35 +0500

    finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -87,7 +87,10 @@ def loader_classification_loss(gpt_model, loader, device, num_batches= None):
 
 def loader_classification_loss_v2(gpt_model, loader, device):
     for input_batch, target_batch in loader:
-        batch_loss= c
+        batch_loss= batch_classification_loss(gpt_model= gpt_model, 
+                                              input_batch= input_batch,
+                                              target_batch= target_batch, 
+                                              device= device)
 #Function to evaluate model--->Find train and validation loader loss
 def evaluate_model(model, train_loader, val_loader, device, eval_iter):
     model.eval()
