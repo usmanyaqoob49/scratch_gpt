3fd257258f1791a9efc1c6b48e2b9e9f2e24a35a
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-04 01:01:24 +0500

    finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -94,3 +94,15 @@ def evaluate_model(model, train_loader, val_loader, device, eval_iter):
         val_loss = loader_classification_loss(val_loader, model, device, num_batches=eval_iter)
     model.train()
     return train_loss, val_loss
+
+#This function will return the gpt model having all layers freezed except the last one
+def freeze_model(gpt_model, num_classes, got_configurations):
+    for params in gpt_model.parameters():
+        params.required_grad()= False
+    gpt_model.out_head= torch.nn.Linear(in_features= got_configurations['emb_dim'],
+                                         out_features= num_classes)
+    for params in gpt_model.out_head.parameters():
+        params.requires_grad()= True
+    for params in gpt_model.final_norm.parameters():
+        params.requires_grad()= True
+    return gpt_model
