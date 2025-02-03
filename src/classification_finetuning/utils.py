a0c4b06a050f51d99746c862b1d792404cac6499
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-04 00:44:29 +0500

    finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -55,4 +55,5 @@ def loader_classification_accuracy(data_loader, model, device, num_batches= None
     return correct_predictions / num_examples
 
 #Function to find the classification loss via cross entropy of a batch
-def batch_classification_loss()
+def batch_classification_loss(input_batch, target_batch, device):
+    
