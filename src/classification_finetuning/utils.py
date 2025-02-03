2d00fca78633872b75b1c30f0644142b55298d3e
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-04 00:52:36 +0500

    finetuning

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -69,7 +69,7 @@ def loader_classification_loss(gpt_model, loader, device, num_batches= None):
     if len(loader) == 0:
         return float('nan')
     elif num_batches == None:
-        
+        num_batches= len(loader)
     gpt_model.eval()
     for input_batch, target_batch in loader:
         input_batch, target_batch= input_batch.to(device), target_batch.to(device)
