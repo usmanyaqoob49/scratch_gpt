52ea1c4b3b255425a9ad018c2f4514b853a580a1
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-04 01:17:58 +0500

    finetuning

--- src/classification_finetuning/finetune.py
+++ src/classification_finetuning/finetune.py
@@ -14,6 +14,7 @@ def finetune_model(model, train_loader, validation_loader, optimizer, device, nu
                                                   input_batch= input_batch,
                                                   target_batch= target_batch,
                                                   device= device)
+            batch_loss.backward()
             
     
 
