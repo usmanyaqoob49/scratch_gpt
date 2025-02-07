a4d0f4269baaaf57bd17ffecbd6fe5ca07192fcb
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-07 23:44:53 +0500

    finetuning function

--- src/classification_finetuning/finetune.py
+++ src/classification_finetuning/finetune.py
@@ -27,7 +27,9 @@ def finetune_model(model, train_loader, validation_loader, optimizer, device, nu
             examples_seen+= input_batch.shape[0]
             global_step+=1  
             if global_step % eval_frequency == 0:
-                train_loss, val_loss= evaluate_model()
+                train_loss, val_loss= evaluate_model(model= model,
+                                                     train_loader= train_loader,
+                                                     )
 
 
 
