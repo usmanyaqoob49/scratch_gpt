256ebd7bbf584d516762f6bcb7443bae49c3c4c6
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-09 22:48:44 +0500

    training function for the finetuning;

--- src/classification_finetuning/train.py
+++ src/classification_finetuning/train.py
@@ -2,10 +2,14 @@
 from .utils import get_gpt_2_openai
 from .finetune import finetune_model
 
-def train(training_loader, validation_loader, optimizer, ):
+def train(training_loader, validation_loader, optimizer, device, num_epochs, eval_frequency, batch_size):
     gpt_2_openai= get_gpt_2_openai()
     training_accuray, validation_accuracy, training_loss, validation_loss, examples_seen= finetune_model(model= gpt_2_openai,
                                                                                                          train_loader= training_loader,
                                                                                                          validation_loader= validation_loader,
-                                                                                                         )
+                                                                                                         optimizer= optimizer,
+                                                                                                         device= device,
+                                                                                                         num_epochs= num_epochs,
+                                                                                                         eval_frequency= eval_frequency,
+                                                                                                         eval_iter= batch_size)
     
