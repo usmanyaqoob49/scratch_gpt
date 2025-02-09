115218c6b4e7b559c29acc7193accfe2da219296
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-09 22:57:42 +0500

    training function for the finetuning;

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -4,6 +4,7 @@ from src.gpt.gpt_model import GPTModel
 from src.pretraining.utils import load_gpt2_params_from_tf_ckpt
 from src.pretraining.loading_weight_into_gpt import loads_weight_into_gpt
 from src.pretraining.utils import gpt_2_124m_configurations
+import matplotlib.pyplot as plt
 
 #Function that will balance the classification dataset based on minimum number of classes present in it (like all class frequence will be equal to lowest repeated class)
 def balance_dataset(dataset_path, classes_column_name):
@@ -132,3 +133,22 @@ def get_gpt_2_openai():
                                      params= openai_gpt2_weights)
     return gpt_2_model
     
+#To plot the loss and accuracy graphs
+def plot_values(epochs_seen, examples_seen, train_values, val_values, label="loss"):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    # Plot training and validation loss against epochs
+    ax1.plot(epochs_seen, train_values, label=f"Training {label}")
+    ax1.plot(epochs_seen, val_values, linestyle="-.", label=f"Validation {label}")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel(label.capitalize())
+    ax1.legend()
+
+    # Create a second x-axis for examples seen
+    ax2 = ax1.twiny()  # Create a second x-axis that shares the same y-axis
+    ax2.plot(examples_seen, train_values, alpha=0)  # Invisible plot for aligning ticks
+    ax2.set_xlabel("Examples seen")
+
+    fig.tight_layout()  # Adjust layout to make room
+    plt.savefig(f"{label}-plot.pdf")
+    plt.show()
