8a1abdc5a9496747c33ebef032ac02fc42a30a46
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-09 22:45:51 +0500

    training function for the finetuning;

--- src/classification_finetuning/train.py
+++ src/classification_finetuning/train.py
@@ -3,11 +3,6 @@ from .utils import get_gpt_2_openai
 from .finetune import finetune_model
 from .data_loader import create_data_loaders
 
-def train(classification_dataset, text_col_name, labels_col_name, batch_size):
-    training_loader, validation_loader= create_data_loaders(dataset= classification_dataset,
-                                                            text_col_name= text_col_name,
-                                                            class_col_name= labels_col_name,
-                                                            batch_size= batch_size,
-                                                            )
+def train(classification_dataset, text_col_name, labels_col_name, batch_size, num_workers):
     gpt_2_openai= get_gpt_2_openai()
     
