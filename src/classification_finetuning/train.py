629aa8138a722797332bedd389ebc1541a7f8aef
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-09 22:46:49 +0500

    training function for the finetuning;

--- src/classification_finetuning/train.py
+++ src/classification_finetuning/train.py
@@ -1,9 +1,8 @@
 """Module that has a function that will get gpt-2 architecture, will load weights, get dataset, and then finetune the model."""
 from .utils import get_gpt_2_openai
 from .finetune import finetune_model
-from .data_loader import create_data_loaders
 
 def train(training_loader, validation_loader):
     gpt_2_openai= get_gpt_2_openai()
-    
+
     
