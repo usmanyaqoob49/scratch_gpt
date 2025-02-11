351afd7e7c0cd6fdecc4c997d850fca762502955
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-11 22:16:23 +0500

    function for classifying

--- src/classification_finetuning/utils.py
+++ src/classification_finetuning/utils.py
@@ -136,19 +136,23 @@ def get_gpt_2_openai():
 #To plot the loss and accuracy graphs
 def plot_values(epochs_seen, examples_seen, train_values, val_values, label="loss"):
     fig, ax1 = plt.subplots(figsize=(5, 3))
-
-    # Plot training and validation loss against epochs
     ax1.plot(epochs_seen, train_values, label=f"Training {label}")
     ax1.plot(epochs_seen, val_values, linestyle="-.", label=f"Validation {label}")
     ax1.set_xlabel("Epochs")
     ax1.set_ylabel(label.capitalize())
     ax1.legend()
-
-    # Create a second x-axis for examples seen
-    ax2 = ax1.twiny()  # Create a second x-axis that shares the same y-axis
-    ax2.plot(examples_seen, train_values, alpha=0)  # Invisible plot for aligning ticks
+    ax2 = ax1.twiny()  
+    ax2.plot(examples_seen, train_values, alpha=0) 
     ax2.set_xlabel("Examples seen")
-
-    fig.tight_layout()  # Adjust layout to make room
+    fig.tight_layout()  
     plt.savefig(f"{label}-plot.pdf")
     plt.show()
+
+#To use the classification finetuned model to classify the text example
+def classify_text_example(finetuned_model, text_example, tokenizer, max_length= None, pad_token_id= 50256):
+    finetuned_model.eval()
+    input_ids= tokenizer.encode(text_example)
+    allowed_context_length= finetuned_model.pos_emb.weight.shape[0]
+    allowed_input_ids= input_ids[:min(max_length, allowed_context_length)]
+    padded_input_ids= input_ids + ([pad_token_id] * (max_length - len(input_ids)))
+    
