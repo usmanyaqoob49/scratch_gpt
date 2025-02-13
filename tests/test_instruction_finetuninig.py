8c320b5d1748b1dd7339f52c7f93ccfdd83d26f7
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-13 23:11:44 +0500

    data loader for instruction dataset

--- tests/test_instruction_finetuninig.py
+++ tests/test_instruction_finetuninig.py
@@ -1,11 +1,12 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.intruction_finetuning.utils import InstructionDataset, custom_collate
+from src.intruction_finetuning.data_loader import create_istructions_data_loader
 from src.data_preparation.utils import gpt_tokenizer
+from src.
 import json
 
-#----------Testing Data Preparation Class
+#----------Testing Data Loader
 with open('./data/raw/instructions_data/instruction-data.json', 'r', encoding= 'utf-8') as file:
     data= json.load(file)
 print('First data sample from the instructions dataset: ', data[0])
@@ -14,14 +15,4 @@ batch = [
     data[1],
 ]
 gpt_2_tokenizer= gpt_tokenizer()
-instruction_dataset= InstructionDataset(data= batch, tokenizer= gpt_2_tokenizer)
-encoded_batch= instruction_dataset.encoded_text
-print("Encoded: ", encoded_batch)
-print('Number of encoded data samples: ', instruction_dataset.__len__())
-print('-'*50)
-
-#---------Testing Custom Collate functiont that pads the examples and make input and target samples from batch
-input_tensors, target_tensors= custom_collate(batch= encoded_batch)
-print('Input Tensors: ', input_tensors)
-print('Output Tensors: ', target_tensors)
-print('-'*50)
+training_loader, validation_loader, test_loader= create_istructions_data_loader(training_dataset=)
