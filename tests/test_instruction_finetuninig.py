df10903a41c06e3a7453426205afa9bf0b8f759f
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-13 22:36:05 +0500

    data added and testing

--- tests/test_instruction_finetuninig.py
+++ tests/test_instruction_finetuninig.py
@@ -5,9 +5,9 @@ from src.intruction_finetuning.utils import InstructionDataset, custom_collate
 from src.data_preparation.utils import gpt_tokenizer
 import json
 
+#----------Testing Data Preparation Class
 with open('./data/raw/instructions_data/instruction-data.json', 'r', encoding= 'utf-8') as file:
     data= json.load(file)
-
 print('First data sample from the instructions dataset: ', data[0])
 batch = [
     data[0],
@@ -16,5 +16,6 @@ batch = [
 gpt_2_tokenizer= gpt_tokenizer()
 instruction_dataset= InstructionDataset(data= batch, tokenizer= gpt_2_tokenizer)
 print("Encoded: ", instruction_dataset.encoded_text)
+print('Number of encoded data samples: ', instruction_dataset.__len__())
 
 
