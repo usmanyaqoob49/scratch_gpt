cc56c389d91872fa195ba42ad9b28954877e0e07
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2025-02-13 23:14:57 +0500

    data loader for instruction dataset

--- tests/test_instruction_finetuninig.py
+++ tests/test_instruction_finetuninig.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.intruction_finetuning.data_loader import create_istructions_data_loader
 from src.data_preparation.utils import gpt_tokenizer
-from src.
+from src.classification_finetuning.utils import train_val_test_split
 import json
 
 #----------Testing Data Loader
@@ -15,4 +15,13 @@ batch = [
     data[1],
 ]
 gpt_2_tokenizer= gpt_tokenizer()
-training_loader, validation_loader, test_loader= create_istructions_data_loader(training_dataset=)
+train_set, validation_set, test_set= train_val_test_split(data= data)
+training_loader, validation_loader, test_loader= create_istructions_data_loader(training_dataset=train_set,
+                                                                                validation_dataset= validation_set,
+                                                                                test_dataset= test_set,
+                                                                                tokenizer= gpt_tokenizer,
+                                                                                batch_size= 8,
+                                                                                num_workers= 0)
+for input, target in training_loader:
+    print('input shape: ', input.shape)
+    print('target shape: ', target.shape)
